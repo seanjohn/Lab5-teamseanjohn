1d1bf6d5635c2fa1e6b9c9b48eb14a48be5f1fde
John Krasich <krasicji@rose-hulman.edu>
2011-03-24 16:10:55 -0400

    New Java Project

---
+++ Lab5/src/Lab5Tests.java
@@ -0,0 +1,22 @@
+
+import org.junit.Before;
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author krasicji.
+ *         Created Mar 24, 2011.
+ */
+public class Lab5Tests {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+}
