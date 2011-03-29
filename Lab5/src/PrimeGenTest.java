23806bf131b1ba3d3027d6d616f3b1c2e592e429
John Krasich <krasicji@rose-hulman.edu>
2011-03-28 21:52:50 -0400

    Created first test for prime generator.

---
+++ Lab5/src/PrimeGenTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+
+public class PrimeGenTest {
+	
+	@Test
+	public void Test1(){
+		assertEquals(new ArrayList<Integer>(),PrimeGenerator.primeGen(1));
+	}
+
+}
