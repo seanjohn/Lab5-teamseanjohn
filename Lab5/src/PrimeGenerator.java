abf2319ad661422a14bba2abfc7b724aadbff074
Sean Cavanaugh <cavanasm@rose-hulman.edu>
2011-03-28 21:55:52 -0400

    Passes test 1

---
+++ Lab5/src/PrimeGenerator.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class PrimeGenerator {
+
+	public static ArrayList<Integer> primeGen(int i) {
+		// TODO Auto-generated method stub
+		return new ArrayList<Integer>();
+	}
+
+	
+	
+}
