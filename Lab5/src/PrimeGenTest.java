a3126c2bbb8a9f79223f1ceed1b9a14d588e9a40
Sean Cavanaugh <cavanasm@rose-hulman.edu>
2011-03-28 22:19:56 -0400

    Passes big number test. Task COMPLETE!

--- Lab5/src/PrimeGenTest.java
+++ Lab5/src/PrimeGenTest.java
@@ -33,6 +33,11 @@ public class PrimeGenTest {
 		assertEquals(createArray(2,3,5),PrimeGenerator.primeGen(6));
 	}
 	
+	@Test
+	public void TestBIGNUMBER(){
+		assertEquals(createArray(2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97),PrimeGenerator.primeGen(100));
+	}
+	
 	public ArrayList<Integer> createArray(int...ints){
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		for(int x: ints){
